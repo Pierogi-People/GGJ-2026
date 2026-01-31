d188a875a6c541d25ce09f68b7637f4bd03e9b16
Freddie Lane <frederick2.lane@live.uwe.ac.uk>
2026-01-31 11:42:51 +0000

    Look up and down added, rotation puzzle based on camera position

--- Assets/Scrips/PlayerMovement.cs
+++ Assets/Scrips/PlayerMovement.cs
@@ -4,14 +4,21 @@ public class PlayerMovement : MonoBehaviour
 {
     #region "Variables"
     private Rigidbody Rigid;
+    private Camera Cam;
     public float MouseSensitivity;
     public float MoveSpeed;
     public float sprintMultiplier;
+    public float upperCameraBounds;
+    public float lowerCameraBounds;
+    private float currentTilt;
+    public float tiltSpeed;
     #endregion
 
     private void Start()
     {
         Rigid = gameObject.GetComponent<Rigidbody>();
+        Cam = Camera.main;
+        currentTilt = 0f;
     }
 
     void Update()
@@ -25,5 +32,16 @@ public class PlayerMovement : MonoBehaviour
 
         Rigid.MoveRotation(Rigid.rotation * Quaternion.Euler(new Vector3(0, Input.GetAxis("Mouse X") * MouseSensitivity, 0)));
         Rigid.MovePosition(transform.position + (transform.forward * Input.GetAxis("Vertical") * (MoveSpeed * multi)) + (transform.right * Input.GetAxis("Horizontal") * (MoveSpeed * multi)));
+
+        currentTilt -= (Input.GetAxis("Mouse Y") * MouseSensitivity * Time.deltaTime * tiltSpeed);
+        if (currentTilt > upperCameraBounds)
+        {
+            currentTilt = upperCameraBounds;
+        }
+        else if (currentTilt < lowerCameraBounds)
+        {
+            currentTilt = lowerCameraBounds;
+        }
+        Cam.transform.localRotation = new Quaternion(currentTilt, 0, 0, 1);
     }
 }
