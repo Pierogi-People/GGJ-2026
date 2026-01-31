5996d809e8a84d910365617ac4bbb6996e0ea5d2
Dan <danielellett@hotmail.com>
2026-01-31 18:29:08 +0000

    Puzzle 2 half-working and clean up
    oh and snorkel

--- Assets/Scrips/onClick.cs
+++ Assets/Scrips/onClick.cs
@@ -4,11 +4,12 @@ public class onClick : MonoBehaviour
 {
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1))
         {
             Ray ray = new Ray (origin: Camera.main.transform.position, direction: Camera.main.transform.forward);
             if (Physics.Raycast(ray, out RaycastHit hit, 500f))
             {
+                Debug.Log("Hit " + hit.transform.name);
                 if (hit.transform.tag == "Puzzle")
                 {
                     hit.transform.rotation *= Quaternion.Euler(0, 0, 90f);
