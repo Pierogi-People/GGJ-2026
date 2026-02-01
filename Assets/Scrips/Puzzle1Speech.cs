42b8ad54ea7623d8df967aefd8968c6123d62807
Freddie Lane <frederick2.lane@live.uwe.ac.uk>
2026-02-01 14:05:57 +0000

    Level 1 speech

---
+++ Assets/Scrips/Puzzle1Speech.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.Rendering;
+
+public class Puzzle1Speech : MonoBehaviour
+{
+    public AudioResource Speech1;
+    public AudioResource Speech2;
+
+    public float speech1Delay = 2f;
+    public bool awaitingSpeech1End = false;
+    private bool speechTriggered = false;
+
+    public float volume = 1f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement>().enabled = false;
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (speech1Delay > 0)
+        {
+            speech1Delay -= Time.deltaTime;
+            if (speech1Delay < 0)
+            {
+                SpeakSpeech1();
+                awaitingSpeech1End = true;
+            }
+        }
+        else if (awaitingSpeech1End)
+        {
+            if (!GameObject.Find("Speech").GetComponent<AudioSource>().isPlaying)
+            {
+                awaitingSpeech1End = false;
+                GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement>().enabled = true;
+            }
+        }
+    }
+
+    private void SpeakSpeech1()
+    {
+        GameObject.Find("Speech").GetComponent<AudioSource>().resource = Speech1;
+        GameObject.Find("Speech").GetComponent<AudioSource>().volume = volume;
+        GameObject.Find("Speech").GetComponent<AudioSource>().Play();
+    }
+
+    public void SpeakSpeech2()
+    {
+        GameObject.Find("Speech").GetComponent<AudioSource>().resource = Speech2;
+        GameObject.Find("Speech").GetComponent<AudioSource>().volume = volume;
+        GameObject.Find("Speech").GetComponent<AudioSource>().Play();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if (!speechTriggered)
+            {
+                GameObject.Find("SpeechDirector").GetComponent<Puzzle1Speech>().SpeakSpeech2();
+                speechTriggered = true;
+            }
+        }
+    }
+}
